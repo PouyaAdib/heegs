41f7125c6c5cf6fe003d2a9e9fcb5d34b596d5c9
Pouya Adib <pouyaadn@gmail.com>
2018-06-16 23:01:31 +0430

    config

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import typescript from 'rollup-plugin-typescript2'
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'es',
+    // name: 'heegs',
+  },
+  plugins: [
+    typescript()
+  ]
+}
